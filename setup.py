140a779e850d72cbc574ca3d703fda561a5414b6
Tim Hunter <timhunter@databricks.com>
2019-04-17 23:34:52 +0200

    fixes

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name='pandas-profiling',
-    version='1.4.1',
+    version='1.4.1b1',
     author='Jos Polfliet',
     author_email='jos.polfliet+panpro@gmail.com',
     packages=['pandas_profiling'],
